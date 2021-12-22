113471f4a6a97bedbfcb013a554e3342f114be6e
Fandy Gotama <gotamafandy@gmail.com>
2021-12-22 17:38:11 +0700

    Initial Commit

---
+++ main/android/app/src/main/kotlin/com/adrena/main/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.adrena.main
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {}
---
+++ main/plugins/flutter_appcenter_bundle/android/src/main/kotlin/com/github/hanabi1224/flutter_appcenter_bundle/FlutterError.kt
@@ -0,0 +1,5 @@
+package com.github.hanabi1224.flutter_appcenter_bundle
+
+import java.lang.Exception
+
+class FlutterError internal constructor(message: String?) : Exception(message)
